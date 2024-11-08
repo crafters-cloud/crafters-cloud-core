5ce5b6c43b3e62df66be91f9c1377c17414a0c08
Srdjan Zivojinovic <srdjan.zivojinovic@crafters.cloud>
2024-11-08 17:40:28 +0100

    Add Carter extensions

---
+++ src/AspNetCore/Carter/ServiceCollectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using Carter;
+using JetBrains.Annotations;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CraftersCloud.Core.AspNetCore.Carter;
+
+[PublicAPI]
+public static class ServiceCollectionExtensions
+{
+    public static void AddCoreCarter(this IServiceCollection services, Assembly[] assembliesWithCarterModules) =>
+        services.AddCarter(configurator: c =>
+        {
+            var carterModules = FindCarterModules(assembliesWithCarterModules);
+            c.WithModules(carterModules);
+        });
+
+    private static Type[] FindCarterModules(Assembly[] assemblies)
+    {
+        var types = assemblies.SelectMany(assembly => assembly.GetTypes());
+        var modules = types
+            .Where(t =>
+                !t.IsAbstract &&
+                typeof(ICarterModule).IsAssignableFrom(t) &&
+                t != typeof(ICarterModule)
+                && t.IsPublic
+            ).ToArray();
+        return modules;
+    }
+}
