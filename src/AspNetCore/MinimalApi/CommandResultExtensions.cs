80c2b540cc7b014a894db2bb8c345c409c0471f8
Srdjan Zivojinovic <srdjan.zivojinovic@crafters.cloud>
2024-11-22 18:50:19 +0100

    And another

--- src/AspNetCore/MinimalApi/CommandResultExtensions.cs
+++ src/AspNetCore/MinimalApi/CommandResultExtensions.cs
@@ -1,26 +1,22 @@
-﻿using CraftersCloud.Core.Results;
+﻿using CraftersCloud.Core.Messaging.CommandResults;
 using JetBrains.Annotations;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
-using OneOf;
-using BadRequestResult = CraftersCloud.Core.Results.BadRequestResult;
-using NoContentResult = CraftersCloud.Core.Results.NoContentResult;
-using NotFoundResult = CraftersCloud.Core.Results.NotFoundResult;
 
 namespace CraftersCloud.Core.AspNetCore.MinimalApi;
 
 [PublicAPI]
-public static class ResultExtensions
+public static class CommandResultExtensions
 {
     public static Results<Created<T>, BadRequest> ToMinimalApiResult<T>(
-        this OneOfBase<CreatedResult<T>, BadRequestResult> command) =>
+        this CreateCommandResult<T> command) =>
         command.Match<Results<Created<T>, BadRequest>>(
             created => TypedResults.Created("", created.Value),
             badRequest => TypedResults.BadRequest());
 
     public static Results<NoContent, NotFound, BadRequest<ValidationProblemDetails>>
-        ToMinimalApiResult<T>(this OneOfBase<NoContentResult, NotFoundResult, BadRequestResult> command) =>
+        ToMinimalApiResult<T>(this UpdateCommandResult<T> command) =>
         command.Match<Results<NoContent, NotFound, BadRequest<ValidationProblemDetails>>>(
             noContent => TypedResults.NoContent(),
             notFound => TypedResults.NotFound(),
