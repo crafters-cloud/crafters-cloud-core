b5af80c25bc8b77f4a97cebd0210b4a4bc65b3b0
Srdjan Zivojinovic <srdjan.zivojinovic@crafters.cloud>
2025-01-03 11:30:38 +0100

    Removing not needed libraries

---
+++ src/TestUtilities/TestTimeProvider.cs
@@ -0,0 +1,16 @@
+﻿namespace CraftersCloud.Core.TestUtilities;
+
+/// <summary>
+/// Allows setting the current time for testing purposes.
+/// </summary>
+[PublicAPI]
+public class TestTimeProvider : ITimeProvider
+{
+    private DateTimeOffset? _nowValue;
+    
+    private readonly Lazy<DateTimeOffset> _now = new(() => DateTimeOffset.UtcNow);
+    public DateTimeOffset FixedUtcNow => _nowValue ?? _now.Value;
+    public DateTimeOffset UtcNow => _nowValue ?? DateTimeOffset.UtcNow;
+
+    public void SetNow(DateTimeOffset value) => _nowValue = value;
+}
