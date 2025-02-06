d35cc79f4b737f76a91ad7b4f968f370f20f85cc
Srdjan Zivojinovic <srdjan.zivojinovic@crafters.cloud>
2025-02-06 10:57:14 +0100

    Workin in progress

---
+++ src/Core.SystemTextJson/JsonConverterExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using System.Text.Json.Serialization;
+using CraftersCloud.Core.StronglyTypedIds;
+
+namespace CraftersCloud.Core.SystemTextJson;
+
+[PublicAPI]
+public static class JsonConverterExtensions
+{
+    /// <summary>
+    /// Register SmartEnum json converters for System.Text.Json
+    /// </summary>
+    /// <param name="converters">List of converters to add converters to</param>
+    /// <param name="assembliesWithStronglyTypedIds">Assemblies containing SmartEnums</param>
+    public static void AddCoreStronglyTypedIdsJsonConverters(this IList<JsonConverter> converters,
+        IEnumerable<Assembly> assembliesWithStronglyTypedIds)
+    {
+        var stronglyTypedIds = assembliesWithStronglyTypedIds.FilterStronglyTypedIds();
+
+        foreach (var values in stronglyTypedIds)
+        {
+            var converterType =
+                typeof(StronglyTypedIdJsonConverter<,>).MakeGenericType(values.Type, values.ValueType);
+            var converter = (JsonConverter) Activator.CreateInstance(converterType)!;
+            converters.Add(converter);
+        }
+    }
+}
