72005ba3525afd19b5356c379fa42b040b1bb349
Srdjan Zivojinovic <srdjan.zivojinovic@crafters.cloud>
2025-02-24 16:32:42 +0100

    Remove app insights

--- src/Infrastructure/ApplicationInsightsSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CraftersCloud.Core.Infrastructure;
-
-[PublicAPI]
-public class ApplicationInsightsSettings
-{
-    public const string SectionName = "ApplicationInsights";
-
-    public string ConnectionString { get; set; } = string.Empty;
-}
